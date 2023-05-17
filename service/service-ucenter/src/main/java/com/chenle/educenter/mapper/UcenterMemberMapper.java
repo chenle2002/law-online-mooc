425fda5edac6c26cc03a520de62f824040993e4f
chenle2002 <chenle2020@mail.ynu.edu.cn>
2023-05-17 19:28:33 +0800

    新增用户中心模块

---
+++ service/service-ucenter/src/main/java/com/chenle/educenter/mapper/UcenterMemberMapper.java
@@ -0,0 +1,17 @@
+package com.chenle.educenter.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chenle.educenter.entity.UcenterMember;
+
+/**
+ * <p>
+ * 会员表 Mapper 接口
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-03-09
+ */
+public interface UcenterMemberMapper extends BaseMapper<UcenterMember> {
+
+}
