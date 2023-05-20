3f1255be3493eeaf5c090922545654e7243686fd
chenle2002 <chenle2020@mail.ynu.edu.cn>
2023-05-20 18:47:32 +0800

    新增统计模块

---
+++ service/service-statistics/src/main/java/com/chenle/staservice/mapper/StatisticsDailyMapper.java
@@ -0,0 +1,17 @@
+package com.chenle.staservice.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chenle.staservice.entity.StatisticsDaily;
+
+/**
+ * <p>
+ * 网站统计日数据 Mapper 接口
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-03-14
+ */
+public interface StatisticsDailyMapper extends BaseMapper<StatisticsDaily> {
+
+}
